0b7886f5e7aec3a2e520edfee5d601a2e11c0fd8
Andres Giner Anton <andresgineranton@outlook.com>
2019-04-30 13:35:13 +0200

    explicit imports

--- pgaudi/main.py
+++ pgaudi/main.py
@@ -20,8 +20,7 @@ from functools import partial
 import gaudi.parse
 
 # Pgaudi
-# from . import parallel, treatment, similarity, create_output
-import parallel, treatment, similarity, create_output
+from . import parallel, treatment, similarity, create_output
 
 
 def run(input_yaml, processes, complexity):
