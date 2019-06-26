049e172e0aa3fc1cf027fff5a6d1e73fc5bb3cf0
Andres Giner Anton <andresgineranton@outlook.com>
2019-06-26 12:57:48 +0200

    add copyright text

--- pgaudi/__init__.py
+++ pgaudi/__init__.py
@@ -1,6 +1,28 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+#############
+#   PGaudi: A package for optimize the performance
+#   of the GaudiMM suite by external parallelization
+
+#   https://github.com/insilichem/pgaudi
+
+#   Copyright 2019 Andrés Giner Antón, Jaime Rodriguez-Guerra
+#   and Jean-Didier Marechal
+
+#   Licensed under the Apache License, Version 2.0 (the "License");
+#   you may not use this file except in compliance with the License.
+#   You may obtain a copy of the License at
+
+#        http://www.apache.org/licenses/LICENSE-2.0
+
+#   Unless required by applicable law or agreed to in writing, software
+#   distributed under the License is distributed on an "AS IS" BASIS,
+#   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#   See the License for the specific language governing permissions and
+#   limitations under the License.
+#############
+
 """
 PGaudi is a package for the optimization of the performance of the GaudiMM suite by external parallelization.
 It consists of five modules:
